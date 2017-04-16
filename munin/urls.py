b08046f9d8aef3d2df9290e2fc1fcb89969d7947
Thomas Merkel <tm@core.io>
2017-04-16 15:47:31 +0200

    Fix URLs

--- munin/urls.py
+++ munin/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
-import views
+from .views import *
 
 urlpatterns = patterns('',
-	url(r'^$',                          TemplateView.as_view(template_name='munin/main.html'), name='main'),
-	url(r'^groups/$',					views.GroupList.as_view(),      name='group_list'),
-	url(r'^groups/add$',				views.GroupCreate.as_view(),    name='group_create'),
-	url(r'^groups/(?P<pk>\d+)/delete$',	views.GroupDelete.as_view(),    name='group_delete'),
-	url(r'^hosts/$',					views.HostList.as_view(),       name='host_list'),
-	url(r'^hosts/add$',					views.HostCreate.as_view(),     name='host_create'),
-	url(r'^hosts/(?P<pk>\d+)/delete$',	views.HostDelete.as_view(),     name='host_delete'),
+	url(r'^$',				TemplateView.as_view(template_name='munin/main.html'), name='main'),
+	url(r'^groups/$',			GroupList.as_view(),      name='group_list'),
+	url(r'^groups/add$',			GroupCreate.as_view(),    name='group_create'),
+	url(r'^groups/(?P<pk>\d+)/delete$',	GroupDelete.as_view(),    name='group_delete'),
+	url(r'^hosts/$',			HostList.as_view(),       name='host_list'),
+	url(r'^hosts/add$',			HostCreate.as_view(),     name='host_create'),
+	url(r'^hosts/(?P<pk>\d+)/delete$',	HostDelete.as_view(),     name='host_delete'),
 )
